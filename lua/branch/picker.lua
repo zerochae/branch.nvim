ba60f849219e089dc3162ede5464f080d7579927
zerochae <zerochae@kakao.com>
2025-02-03 10:29:55 +0900

    fix(#master): opts

--- lua/branch/picker.lua
+++ lua/branch/picker.lua
@@ -3,17 +3,14 @@ local branch_finder = require "branch.finder"
 local branch_previewer = require "branch.previewer"
 local branch_mappings = require "branch.mappings"
 local conf = require("telescope.config").values
-local state = require "branch.state"
 
-return function(opts)
-  opts = opts or state
-
+return function(state)
   return pickers
-    .new(opts, {
-      prompt_title = opts.prompt.title,
-      preview_title = opts.preview.title,
-      finder = branch_finder(opts.finder),
-      previewer = branch_previewer(opts.preview),
+    .new({}, {
+      prompt_title = state.prompt.title,
+      preview_title = state.preview.title,
+      finder = branch_finder(state.finder),
+      previewer = branch_previewer(state.preview),
       sorter = conf.generic_sorter(),
       attach_mappings = branch_mappings,
     })
